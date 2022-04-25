44b9ef636325bb04e9b277e4425b768be82b1986
Ryan Kenney <rkenney525@gmail.com>
2022-04-25 11:09:46 -0400

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # tesults-aggregate-upload-action
 GitHub Action to aggregate and upload test results data to Tesults
+
+## Assumptions
+TODO this
+
+## Inputs
+TODO this
+
+## Outputs
+TODO this
+
+## Examples
+TODO this
---
+++ index.js
@@ -0,0 +1,43 @@
+const core = require('@actions/core');
+const fs = require('fs');
+const { exit } = require('process');
+const tesults = require('tesults');
+
+const token = core.getInput('target_token', { required: true });
+const dataDir = core.getInput('test_data_directory', { required: true });
+
+const createTestData = (filename) => {
+    let rawdata = fs.readFileSync(filename);
+    let testData = JSON.parse(rawdata);
+    let suite = testData.results.file.split('/').slice(-1);
+    let tests = testData.results.tests.concat(testData.results.suites.flatMap( s => s.tests))
+    // TODO support attachments
+    return tests.map(test => {
+        return {
+            name: test.fullTitle,
+            suite: suite,
+            duration: test.duration,
+            result: test.pass ? 'pass' : 'fail',
+        }
+    });
+}
+
+let cases = fs.readdirSync(dataDir).flatMap(createTestData);
+
+let data = {
+    target: token,
+    results: {
+        cases: cases
+    }
+};
+
+tesults.results(data, function (err, response) {
+    console.log("upload to tesults result: " + response.success);
+    
+    if (err) {
+        console.log("Tesults response: " + response.message)
+        console.log(err);
+    }
+
+    exit(response.success ? 0 : 1);
+});
