9bb19438074c5bfe49fd55a7e3e86ac5241ecfd9
Ryan Kenney <rkenney525@gmail.com>
2022-05-18 14:36:03 -0400

    re-add debug

--- index.js
+++ index.js
@@ -110,6 +110,8 @@ const cases = fs.readdirSync(dataDir)
       .map(filename => `${dir}/${filename}`)
       .map(filename => supportedFormats[dir].processor(filename)));
 
+fs.readdirSync(dataDir).forEach(dir => console.log(dir));
+
 console.log('Sending the following test case data to tesults');
 console.log(JSON.stringify(cases))
 
