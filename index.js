b1c627218852d0b5becd3616c7a1e72062d3c0d7
Ryan Kenney <rkenney525@gmail.com>
2022-05-19 11:11:04 -0400

    Support junit and mochawesome (#2)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const core = require('@actions/core');
 const fs = require('fs');
 const { exit } = require('process');
 const tesults = require('tesults');
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser();
 
 const token = core.getInput('target_token', { required: true });
 const dataDir = core.getInput('test_data_directory', { required: true });
@@ -14,7 +16,8 @@ function getTests(suite)  {
     }
 }
 
-const createTestData = (filename) => {
+// TODO turn these into classes in other files maybe?
+const createMochawesomeTestData = (filename) => {
     const fullName = `${dataDir}/${filename}`
     console.log(`processing test file: ${fullName}`);
 
@@ -44,11 +47,71 @@ const createTestData = (filename) => {
             }
         });
     });
-}
+};
+
+const createJunitTestData = (filename) => {
+    const fullName = `${dataDir}/${filename}`
+    console.log(`processing test file: ${fullName}`);
+
+    const rawdata = fs.readFileSync(fullName);
+    let testData = [];
+    parser.parseString(rawdata, function(_, data) {
+        testData = data.testsuite.testcase.map(testcase => {
+            let files = [];
+            const screenshot = fullName.replace('.xml', '.png');
+            if (fs.existsSync(screenshot)) {
+                files.push(screenshot);
+            }
+            if (testcase["std-out"] != null && testcase["std-out"].length > 0) {
+                const newFilePath = `${fullName}.stdout.log`;
+                fs.writeFileSync(newFilePath, testcase["std-out"][0]);
+                files.push(newFilePath);
+            }
+            if (testcase["std-err"] != null && testcase["std-err"].length > 0) {
+                const newFilePath = `${fullName}.stderr.log`;
+                fs.writeFileSync(newFilePath, testcase["std-err"][0]);
+                files.push(newFilePath);
+            }
+
+
+            let failureReason = "";
+            if (testcase.failure != null && testcase.failure.length > 0) {
+                failureReason = testcase.failure[0].$message;
+            } else if (testcase.error != null && testcase.error.length > 0) {
+                failureReason = testcase.error[0].$message;
+            }
+
+            return {
+                name: testcase.$.name,
+                suite: testcase.$.classname,
+                reason: failureReason != "" ? failureReason : null,
+                duration: parseFloat(testcase.$.time) * 1000,
+                result: failureReason != "" ? 'fail' : 'pass',
+                files: files,
+            }
+        });
+    });
+    return testData;
+};
+
+const supportedFormats = {
+    mochawesome: {
+        ext: '.json',
+        processor: createMochawesomeTestData,
+    },
+    junit: {
+        ext: '.xml',
+        processor: createJunitTestData,
+    },
+};
 
 const cases = fs.readdirSync(dataDir)
-    .filter(filename => filename.endsWith('.json'))
-    .flatMap(createTestData);
+    .filter(dir => supportedFormats[dir] !== undefined)
+    .flatMap(dir => fs.readdirSync(`${dataDir}/${dir}`)
+      .filter(filename => filename.endsWith(supportedFormats[dir].ext))
+      .map(filename => `${dir}/${filename}`)
+      .flatMap(filename => supportedFormats[dir].processor(filename)));
+
 
 console.log('Sending the following test case data to tesults');
 console.log(JSON.stringify(cases))
